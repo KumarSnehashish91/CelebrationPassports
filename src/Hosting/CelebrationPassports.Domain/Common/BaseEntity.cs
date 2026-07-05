4f5d9f80509d384504e1082d1891e7ffcbd8318f
KumarSnehashish91 <KumarSnehashish91@gmail.com>
2026-07-05 10:43:09 +0530

    Initialize Clean Architecture solution

---
+++ src/Hosting/CelebrationPassports.Domain/Common/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using CelebrationPassports.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CelebrationPassports.Domain.Common
+{
+    public abstract class BaseEntity : IAuditableEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ src/Hosting/CelebrationPassports.Domain/Interfaces/IAuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CelebrationPassports.Domain.Interfaces
+{
+    public interface IAuditableEntity
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
