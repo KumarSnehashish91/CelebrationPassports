078ef3e893c584bb81c530a533778b5aafc5a86a
KumarSnehashish91 <KumarSnehashish91@gmail.com>
2026-07-05 11:01:59 +0530

    Implemented Dependency Injection

---
+++ src/Hosting/CelebrationPassports.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CelebrationPassports.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // MediatR
+            // FluentValidation
+            // AutoMapper
+
+            return services;
+        }
+    }
+}
---
+++ src/Hosting/CelebrationPassports.Persistence/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CelebrationPassports.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
